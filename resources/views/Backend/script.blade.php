19d50f129f9e27748b3027f2aad601b1e229a5f8
MohammedSalama <mohamed96salama@gmail.com>
2023-03-08 14:46:02 +0200

    Backend-Theme

---
+++ resources/views/Backend/script.blade.php
@@ -0,0 +1,38 @@
+<!-- jquery -->
+<script src="{{ URL::asset('Backend/assets/js/jquery-3.3.1.min.js') }}"></script>
+
+<!-- plugins-jquery -->
+<script src="{{ URL::asset('Backend/assets/js/plugins-jquery.js') }}"></script>
+
+<!-- plugin_path -->
+<script>var plugin_path = '{{ asset('Backend/assets/js') }}/';</script>
+
+<!-- chart -->
+<script src="{{ URL::asset('Backend/assets/js/chart-init.js') }}"></script>
+
+<!-- calendar -->
+<script src="{{ URL::asset('Backend/assets/js/calendar.init.js') }}"></script>
+
+<!-- charts sparkline -->
+<script src="{{ URL::asset('Backend/assets/js/sparkline.init.js') }}"></script>
+
+<!-- charts morris -->
+<script src="{{ URL::asset('Backend/assets/js/morris.init.js') }}"></script>
+
+<!-- datepicker -->
+<script src="{{ URL::asset('Backend/assets/js/datepicker.js') }}"></script>
+
+<!-- sweetalert2 -->
+<script src="{{ URL::asset('Backend/assets/js/sweetalert2.js') }}"></script>
+
+<!-- toastr -->
+<script src="{{ URL::asset('Backend/assets/js/toastr.js') }}"></script>
+
+<!-- validation -->
+<script src="{{ URL::asset('Backend/assets/js/validation.js') }}"></script>
+
+<!-- lobilist -->
+<script src="{{ URL::asset('Backend/assets/js/lobilist.js') }}"></script>
+
+<!-- custom -->
+<script src="{{ URL::asset('Backend/assets/js/custom.js') }}"></script>
